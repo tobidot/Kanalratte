d33fd80f309e1eb12cc308e1aca711ba49cbdbdf
tobidot <Object.name@live.de>
2016-01-23 18:49:17 +0100

    removed trash

--- src/menues/main/MainMenuPresenter.java
+++ src/menues/main/MainMenuPresenter.java
@@ -11,12 +11,10 @@ public class MainMenuPresenter extends Presenter<MainMenuView>
     public MainMenuPresenter(MainPresenter mainP, Model model)
     {
         super(mainP, model, new MainMenuView());
-        ImagePattern[] menue = new ImagePattern[5];
-        menue[0] = null;
+        ImagePattern[] menue = new ImagePattern[6];
+        menue[0] = new ImagePattern(model.getImage("TEST"), 0, 0, 1, 1, true);
         menue[1] = new ImagePattern(model.getImage("MENUE_LEFT"), 0, 0, 1, 1, true);
         menue[2] = new ImagePattern(model.getImage("MENUE_RIGHT"), 0, 0, 1, 1, true);
-        menue[3] = null;
-        menue[4] = null;
         view.setImageSet(menue);
     }
 
--- src/menues/main/MainMenuView.java
+++ src/menues/main/MainMenuView.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -14,47 +15,25 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import mvp.View;
 
-public class MainMenuView extends View<VBox>
+public class MainMenuView extends View<Pane>
 {
     private HBox headMenue;
 
-    private HBox bodyMenue;
-
-    private Pane footer;
-
     private VBox optionsContainer;
-
-    private Rectangle leftImage;
-
-    private Rectangle rightImage;
 
     public MainMenuView()
     {
-        root = new VBox();
+        root = new Pane();
         root.setPrefHeight(Integer.MAX_VALUE);
         root.getChildren().add(new Label("MainMenu"));
         root.setBackground(new Background(new BackgroundFill(Color.WHEAT, new CornerRadii(0), new Insets(0))));
+        root.maxHeightProperty().bind(getResolutionHeight());
         /// Oberste Struktur
         root.getChildren().add(headMenue = new HBox());
-        root.getChildren().add(bodyMenue = new HBox());
-        root.getChildren().add(footer = new Pane());
-        headMenue.setPrefHeight(1000);
-        bodyMenue.setPrefHeight(1000);
-        footer.setPrefHeight(800);
-        /// Body
-        bodyMenue.getChildren().add(leftImage = new Rectangle());
-        bodyMenue.getChildren().add(optionsContainer = new VBox(20));
-        bodyMenue.getChildren().add(rightImage = new Rectangle());
-
-        leftImage.widthProperty().bind(this.getResolutionWidth().multiply(0.35));
-        leftImage.heightProperty().bind(optionsContainer.heightProperty());
-        rightImage.widthProperty().bind(this.getResolutionWidth().multiply(0.35));
-        rightImage.heightProperty().bind(optionsContainer.heightProperty());
-
-        leftImage.setFill(Color.BLACK);
-        optionsContainer.setBackground(new Background(new BackgroundFill(Color.WHEAT, new CornerRadii(0), new Insets(0))));
-        rightImage.setFill(Color.BLACK);
-        ///
+        root.getChildren().add(optionsContainer = new VBox(20));
+        headMenue.setPrefHeight(200);
+        optionsContainer.layoutXProperty().bind(getResolutionWidth().multiply(0.35));
+        optionsContainer.layoutYProperty().bind(getResolutionHeight().multiply(0.2));
 
         /// Testen
         Button b;
@@ -67,10 +46,8 @@ public class MainMenuView extends View<VBox>
      * Position im Array - Erscheinen im Menü
      * <ul>
      * <li>0 - Oberste Zeile "Header Bild"
-     * <li>1 - links von der Optionen Wahl
-     * <li>2 - rechts von der OPtionen Wahl
-     * <li>3 - hinter den Optionen
-     * <li>4 - am unteren rand "Footer"
+     * <li>1 - hinter den Optionen
+     * <li>2 - Bild hinter dem ganzem Menü
      * <ul>
      * 
      * @param images
@@ -78,10 +55,16 @@ public class MainMenuView extends View<VBox>
      */
     public void setImageSet(ImagePattern... images)
     {
-        if (images == null || images.length < 5)
+        if (images == null || images.length < 3)
         {
-            leftImage.setFill(images[1]);
-            rightImage.setFill(images[2]);
+            return;
+        }
+        else
+        {
+            // leftImage.setFill(images[1]);
+            // rightImage.setFill(images[2]);
+            headMenue.setBackground(new Background(new BackgroundFill(images[0], new CornerRadii(0), new Insets(0))));
+            root.setBackground(new Background(new BackgroundFill(images[2], new CornerRadii(0), new Insets(0))));
         }
     }
 
