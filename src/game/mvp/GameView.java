7d86450486b0d703c21d3e736e2dca649cabb0f3
tobidot <Object.name@live.de>
2016-02-01 19:36:13 +0100

    make all Layoutsizes dependent on this.getResolutionHeight / Width

--- src/game/mvp/GameView.java
+++ src/game/mvp/GameView.java
@@ -41,19 +41,19 @@ public class GameView extends View<HBox, GamePresenter>
         /// main Layout
         root.getChildren().add(left = new VBox());
         root.getChildren().add(gameMenu = new GridPane());
+        root.maxHeightProperty().bind(getResolutionHeight());
         root.setFillHeight(true);
-        root.setMaxHeight(600);
-        left.setPrefWidth(5000);
+
+        /// left / right
+        left.prefWidthProperty().bind(this.getResolutionWidth().multiply(0.75));
+        gameMenu.prefWidthProperty().bind(this.getResolutionWidth().multiply(0.25));
+
         /// ingameScreen Layout
         left.getChildren().add(gameScreen = new Pane());
         left.getChildren().add(statusInfo = new GridPane());
-        gameScreen.setMinSize(300, 200);
-        gameScreen.setPrefSize(800, 600);
+        // TODO apply Layout
+
         /// GameMenu Layout
-
-        gameMenu.setPrefWidth(4500);
-        gameMenu.setPrefHeight(Integer.MAX_VALUE);
-
         /// GridPane
         DoubleBinding perc10 = getResolutionHeight().multiply(0.1);
         DoubleBinding perc20 = getResolutionHeight().multiply(0.2);
@@ -70,6 +70,7 @@ public class GameView extends View<HBox, GamePresenter>
             r.maxHeightProperty().bind(perc10);
             r.prefHeightProperty().bind(perc10);
             r.minHeightProperty().bind(perc10);
+            r.setFillHeight(false);
         }
         rows.add(r = new RowConstraints());
         r.maxHeightProperty().bind(perc40);
@@ -79,13 +80,13 @@ public class GameView extends View<HBox, GamePresenter>
         /// Buttons add
         Button b;
         gameMenu.add(b = exitButton = new Button("Exit"), 0, 0, 2, 1);
-        exitButton.setPrefHeight(1000);
-        exitButton.setPrefWidth(1000);
-        // exitButton.setMaxHeight(1000);
+        exitButton.setPrefHeight(Integer.MAX_VALUE);
+        exitButton.setPrefWidth(Integer.MAX_VALUE);
+        exitButton.setMaxHeight(Integer.MAX_VALUE);
         gameMenu.add(b = new Button("INFO"), 0, 5, 2, 1);
-        b.setPrefHeight(1000);
-        b.setPrefWidth(1000);
-        b.setMaxHeight(1000);
+        b.setPrefHeight(Integer.MAX_VALUE);
+        b.setPrefWidth(Integer.MAX_VALUE);
+        b.setMaxHeight(Integer.MAX_VALUE);
 
         gameMenuButton = new ArrayList<Button>();
         addGameMenuButton("Feed");
@@ -94,15 +95,21 @@ public class GameView extends View<HBox, GamePresenter>
             addGameMenuButton("House" + i);
     }
 
+    /**
+     * fügt dem Ingame Auswahl-Menü eine weitere Option hinzu
+     * 
+     * @param cap
+     *            Beschreibung des Buttons
+     */
     private void addGameMenuButton(String cap)
     {
         if (gameMenuButton.size() >= 8)
             return;
         Button b = new Button(cap);
-        b.setPrefHeight(1000);
-        b.setPrefWidth(1000);
-        b.setMaxHeight(1000);
-        b.setMaxWidth(1000);
+        b.prefHeightProperty().bind(this.getResolutionHeight().multiply(0.09));
+        b.setPrefWidth(Integer.MAX_VALUE);
+        b.setMaxHeight(Integer.MAX_VALUE);
+        b.setMaxWidth(Integer.MAX_VALUE);
         gameMenu.add(b, (gameMenuButton.size()) % 2, (gameMenuButton.size()) / 2 + 1);
         System.out.println(gameMenuButton.size());
         gameMenuButton.add(b);
@@ -111,8 +118,6 @@ public class GameView extends View<HBox, GamePresenter>
     protected void setImages(Model model)
     {
         gameMenu.setBackground(new Background(new BackgroundFill(Color.YELLOW, new CornerRadii(0), new Insets(0))));
-        // exitButton.setBackground(new Background(new BackgroundFill(new
-        // ImagePattern(model.getImage("MENUE_LEFT"), 0, 0, 1, 1, true), new
-        // CornerRadii(0), new Insets(0))));
+        exitButton.setBackground(new Background(new BackgroundFill(new ImagePattern(model.getImage("MENUE_LEFT"), 0, 0, 1, 1, true), new CornerRadii(0), new Insets(0))));
     }
 }
